4325f71ad1b0206ddffcb7cdc39b2d37fb7a2f6c
rupesh-ravunny <40589279+rupesh-ravunny@users.noreply.github.com>
2020-10-28 22:05:44 -0400

    Add project files.

---
+++ NCAT_V2/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NCAT_V2.Models
+{
+    public class Customer
+    {
+        public string ssn { get; set; }
+        public string firstName { get; set; }
+    }
+}
---
+++ NCAT_V2/Repositories/dataAccess.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using NCAT_V2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+
+namespace NCAT_V2.Repositories
+{
+    public class dataAccess
+    {
+        String connString = "SERVER=localhost;Port=4406;DATABASE=ncatbank;UID=root;PASSWORD=@Rupeshkr01";
+        public dataAccess()
+        {
+            MySqlConnection mysql = new MySqlConnection(connString);
+            MySqlCommand cmd = new MySqlCommand("select ssn,firstName from ncatbank.customer", mysql);
+
+            mysql.Open();
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            var customers = new List<Customer>();
+            var customer = new Customer();
+            foreach(DataRow item in dt.Rows)
+            {
+                customer.firstName = item[1].ToString();
+                customer.ssn = item[0].ToString();
+                customers.Add(customer);
+                //customers.Add(item.ItemArray.GetValue("ssn");
+            }
+            mysql.Close();
+        }
+    }
+}
